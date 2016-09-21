4442052df7aa25cc703f26e719014ef42d25da12
Alexandr Ivanov <aivanov@informatica.ru>
2016-09-21 14:03:36 +0300

    настройки подключений

---
+++ default-connections.js
@@ -0,0 +1,11 @@
+//пример настроек. на основе этого объекта создать connection.js
+
+module.exports = {
+  arcgis: {
+    1: {
+      servicesUrl: '',
+      username: '',
+      password: ''
+    }
+  }
+};
