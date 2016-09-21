46b332b44bfe9629b37215c2749d0b62f6afaa7b
Alexandr Ivanov <aivanov@informatica.ru>
2016-09-21 09:21:48 +0300

    добавление файла настроек

---
+++ options.js
@@ -0,0 +1,42 @@
+const version = require('./package.json').version;
+const optionsSpec = {
+  sourceDirPath: {
+    required: true,
+    default: 'data/',
+    env: 'BTE_SOURCE_DIR_PATH'
+  },
+  dbType: {
+    required: true,
+    default: 'nedb', // nedb, mongodb
+    env: 'BTE_DB_TYPE'
+  },
+  PORT: {
+    required: true,
+    default: '3001',
+    env: 'BTE_PORT'
+  }
+};
+
+let options = {
+  version
+};
+
+export default {...options, ...Object.keys(optionsSpec).map((key) => {
+  if (!optionsSpec[key].preprocess) {
+    optionsSpec[key].preprocess = function preprocess (str) {
+      return str;
+    };
+  }
+  const opt = { name: key };
+  if (process.env[optionsSpec[key].env]) {
+    opt.value = optionsSpec[key].preprocess(process.env[optionsSpec[key].env]);
+  } else if (optionsSpec[key].default) {
+    opt.value = optionsSpec[key].preprocess(optionsSpec[key].default);
+  } else if (optionsSpec[key].required) {
+    throw new Error('!!! REQUIRED OPTION NOT SET: ' + key);
+  }
+  return opt;
+}).reduce((prev, cur) => {
+  prev[cur.name] = cur.value;
+  return prev;
+}, {})};
